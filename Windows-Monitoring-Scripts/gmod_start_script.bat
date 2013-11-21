2d33e5aca1c2ac5ad33239f76c76fd30874cd435
ab0mbs <smculligan26@gmail.com>
2013-11-21 10:05:48 -0500

    Update gmod_start_script.bat

--- Windows-Monitoring-Scripts/gmod_start_script.bat
+++ Windows-Monitoring-Scripts/gmod_start_script.bat
@@ -1,4 +1,4 @@
 @echo off
-REM This is the start script that will call the powershell script
+REM This is the start script that will call the powershell script. This should be placed in your root garrysmod folder
 title Gmod Restart Script
 powershell -ExecutionPolicy Unrestricted -File "gmod_start.ps1"
