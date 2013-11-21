c7c115f6ea8f0d24e9895ede93c78549128e07cf
ab0mbs <smculligan26@gmail.com>
2013-11-21 10:25:02 -0500

    Update gmod_start.ps1

--- Windows-Monitoring-Scripts/gmod_start.ps1
+++ Windows-Monitoring-Scripts/gmod_start.ps1
@@ -2,10 +2,17 @@ function StartGmod
 {
 $app = Start-Process C:\srcds\orangebox\srcds.exe -ArgumentList "-console -condebug -game garrysmod  +map gm_construct.bsp -maxplayers 20 -autoupdate -port 27016 -secure -verify_all -passthru -WindowStyle Maximized
 $result = Get-Process -id $app.id
+
+# This sets what processor to run the process on
 $result.ProcessorAffinity = 2
+
+# This sets the class of the process
 $result.PriorityClass = "High"
+
 # This saves the pid of the gmod server to a text file and publishes it on the IIS server setup.
 echo $app.id | out-file -encoding ASCII C:\inetpub\wwwroot\gmod_pid.txt
+
+# This waits for the process to finish. In this case, waits for the Gmod server to crash.
 Wait-Process $app.id
 
 # Call the update script here. This is an optional section that can call an update script whenever the server crashes
